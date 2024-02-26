7a9ce358d2b21bc09e068b8f6686bbd5091496aa
João Vitor <jv0488598@gmail.com>
2024-02-26 15:49:03 -0300

    asdmank dojiasn djiosdnk ajiodnk asjiodnk sa

--- src/libs/connection.js
+++ src/libs/connection.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 
-const conn = mysql2.createConnection({
+const conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
